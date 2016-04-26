a83b4e684ecfeba445bd3545e4fa489433ca5b93
jmacvey <macve004@d.umn.edu>
2016-04-26 16:13:41 +0000

    Robotics Demos

---
+++ Bluetooth_Demo/Bluetooth_Demo.ino
@@ -0,0 +1,214 @@
+//---------------------------------------------------------------------------------
+// Arduino Program - Bluetooth HC-06 Demo
+// @author - A man of wealth and taste
+//---------------------------------------------------------------------------------
+
+/* This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+    */
+
+// @brief: Bluetooth Demo that receives commands via serial connection
+// and prints a mode to the LCD.  Users can modify the commands if more are sought.
+
+//---------------------------------------------------------------------------------
+// Hardware Pin Setup
+
+// All pinouts affiliated with Arduino Sensor Shield v5.0 
+// @https://www.google.com/search?q=arduino+sensor+shield+v5+0+schematic&espv=2&biw=1366&bih=643&tbm=isch&imgil=dD7uc3MRnrh25M%253A%253B_gEggaphUxAm8M%253Bhttp%25253A%25252F%25252Fforum.arduino.cc%25252Findex.php%25253Ftopic%2525253D114307.0&source=iu&pf=m&fir=dD7uc3MRnrh25M%253A%252C_gEggaphUxAm8M%252C_&usg=__18YXtbn8lLZ1tRWzxEtq1WrUFrs%3D&ved=0ahUKEwizyKb-hqvMAhXI7iYKHZpDDJQQyjcINA&ei=QbceV_OjOMjdmwGah7GgCQ#imgrc=dD7uc3MRnrh25M%3A
+
+// On bluetooth interface:
+// Vcc -> Vcc
+// RX -> D1
+// TX -> D0
+// Gnd -> Gnd
+
+//---------------------------------------------------------------------------------
+// Required Libraries - THESE MUST BE INSTALLED OR YOU WILL GET ERRORS!
+
+// - J. MacVey's LCDManager class that wraps around the Malpartida's 
+// to simplify LCD writing and display
+//  @https://github.com/jmacvey/Robotics
+// Download the zip, extract the LCDManager folder, 
+// and install in your Arduino/libraries folder
+// Edit -> Include Libaries -> Manage Libaries... to update paths
+
+//  - F. Malpartida's LiquidCrystal_I2C library for LCD-I2C bus
+//  @https://bitbucket.org/fmalpartida/new-liquidcrystal/wiki/Home
+// Extract contents of zipped file and place into your local Arduino/libraries folder.
+// Be sure to either restart the Arduino IDE or click
+// Edit -> Include Libaries -> Manage Libaries... to update paths
+
+// - S. Monk's Timer library to demonstrate "pseudo-asynchronous" threading in Arduino
+//  @http://playground.arduino.cc/Code/Timer
+// Scroll down to installation and click download, 
+// follow same steps as you did with LCD_I2C lib
+
+//---------------------------------------------------------------------------------
+// Instructions
+
+// Upload code to the board and connect to the HC-06 bluetooth module with a bluetooth terminal (default pw 1234 or 0000)
+// emulator application.  Type commands to the device as specified in the command list.
+// This demo was tested with Android's free "Bluetooth Terminal" by Qwerty (free from the Google Play Store)
+
+//---------------------------------------------------------------------------------
+// Potential Errors and Solutions:
+
+// Error: 
+//    out of sync
+// Explanation: 
+//    You are likely trying to upload to the board with the sensor shield attached
+// This is a problem insofar the upload process utilizes D0/D1 (Rx/Tx) on the Arduino
+// and if the sensor rx/tx pins are plugged in during upload, you get async error
+// Solution:
+//    Unplug the motor shield while you upload code to the board.  Then replug the board in.
+
+// Error:
+//    Compilation error -- "Wire.h" not found or undefined references for I2C library
+// Explanation:
+//    Arduino linkers on IDE's before 1.6.6 are fairly stupid - they don't know how to find 
+// "hardware-associated" libraries when included in custom libaries or files
+// Solution:
+//    Update your IDE to latest version via Arduino website
+
+// Error:
+//    Connected to bluetooth module but not receiving any response in the terminal when typing 
+// commands.
+// Explanation:
+//    (1) Faulty or broken device (these devices are easily damaged if over 3.3V is supplied to rx/tx pins for
+// a long time)
+//    (2) Tried to use software serial through the sensor board on pins other than 0/1 (doesn't work)
+// Solutions
+//    (1) Replace the device
+//    (2) Ensure the BLUETOOTH_RX and BLUETOOTH_TX in Config.h is defined as 1 and 0, respectively.
+
+// ALL OTHER ERRORS:
+// Explanation:
+//    You've likely wired something up wrong
+// Solution 1:
+//    Close your eyes, breath deeply, admit your mistake, and reflect on how you can 
+// correct your mistakes.
+// Solution 2:
+//    Yell "God damnit," drink 3 beers, and forget about this until someone else fixes
+// it for you.
+
+//---------------------------------------------------------------------------------
+// Things to note when modifying the file
+
+// You can't enable virtual serial ports through the sensor shield.  
+// Tried with nearly all the digital I/O pins and the only one that 
+// seems to work is a software serial throughpins D1 and D0, which 
+// technically already hardwired from the sensor shield to the Arduino.
+
+// Also, users should use the Timer class to facilitate "async" checking
+// of the bluetooth module.  This is good practice since you can't tie the 
+// TX/RX pins to interrupts in any way that is convenient.
+
+// What this demo will do is allow you to control the robot based on string
+// commands through a simple bluetooth terminal emulator that can be found on 
+// iOS or Android stores (i.e. you don't need to write their own mobile app to 
+// incorporate bluetooth stuff). 
+
+// To amend the demo to facilitate other commands, it's as simple as adding a string 
+// to the command and extra else-if case to the "updateBluetooth" method.  
+// Calling timer.update() in the PID loop will also enable that "pseudo-interrupt" 
+// style to stop/start the robot or put it into different modes.  
+
+//---------------------------------------------------------------------------------
+// Includes
+
+#include <Wire.h>
+#include <SoftwareSerial.h>
+#include "LCDManager.h"
+#include "Config.h"
+#include "Timer.h"
+
+//---------------------------------------------------------------------------------
+// Bluetooth Commands - To implement more
+
+const String BLUETOOTH_HELP   = "HELP";
+const String WALL_FOLLOW_MODE = "WFM";
+const String LINE_FOLLOW_MODE = "LFM";
+
+//---------------------------------------------------------------------------------
+// Forward Declarations
+
+void updateBluetooth();
+
+//---------------------------------------------------------------------------------
+// Globals
+
+// Creates a virtual serial port
+// You can change the pins to D0-D12 if you are not using the v5.0 sensor shield
+// Otherwise you should keep values for Bluetooth RX and TX at 1 and 0, respectively
+SoftwareSerial BT(BLUETOOTH_RX, BLUETOOTH_TX); 
+LCDManager* lcdManager;
+Timer* timer; // use timer since interrupt pins are inconvenient, kinda like margarine
+
+//---------------------------------------------------------------------------------
+// Initialization
+
+void setup()  
+{
+  BT.begin(9600);
+  lcdManager = new LCDManager();
+  timer = new Timer();
+  timer->every(BLUETOOTH_DELAY, updateBluetooth);
+}
+
+
+//---------------------------------------------------------------------------------
+// Main Program
+
+void loop() 
+{
+  // required.  (takes ~1-20us to execute)
+  // Stupid question: why use the timer?
+  // Simple answer: because you can always pass a reference to it
+  // to other modules to enable a pseudo-interrupt at little cost
+  // to memory and execution time
+  timer->update();
+}
+
+
+void updateBluetooth() {
+  // if text arrived in from BT serial...
+  if (BT.available())
+  {
+    // note we use if/else here because arduino can't switch/case over strings
+    String stringBuffer = BT.readString();
+    if (stringBuffer.equals(LINE_FOLLOW_MODE))
+    {
+      lcdManager->printStringToLCD("Line Follower Mode Enabled");
+      BT.println("Line Follower Mode Enabled");
+    }
+    else if (stringBuffer.equals(WALL_FOLLOW_MODE))
+    {
+      lcdManager->printStringToLCD("Wall Follower Mode Enabled");
+     // digitalWrite(A1, LOW);
+      BT.println("Wall Follower Mode Enabled");
+    }
+    else if (stringBuffer.equals(BLUETOOTH_HELP))
+    {
+      BT.println("Commands:");
+      BT.println("WFM -> Enable Wall Follow Mode");
+      BT.println("LFM -> Enable Line Follow Mode");
+    }
+    // TO DO : Implement your own commands
+    else
+    {
+      BT.println("Command not recognized.");
+      BT.println("Type \"HELP\" to see available commands");
+    }
+  }
+}
+
---
+++ LCD_Demo/LCD_Demo.ino
@@ -0,0 +1,196 @@
+//---------------------------------------------------------------------------------
+// Arduino Program - LCD Display Demo using I2C Bus and F. Malpartida's
+// LiquidCrystal_I2C libary
+// @author - J. MacVey
+//---------------------------------------------------------------------------------
+
+/* This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+    */
+
+// @brief: prints whatever you type in the Arduino Serial console
+// to the LCD display using the I2C bus.  Change baud_rate, i2c pins,
+// and num chars to reflect your device layout
+// If you have trouble compiling, you probably need to upgrade your Arduino IDE
+// to the latest version -- old version linkers are t-e-r-r-i-b-l-e
+
+// Download F. Malpartida's LiquidCrystal_I2C libary 
+// @https://bitbucket.org/fmalpartida/new-liquidcrystal/wiki/Home
+// Extract contents and place into your local Arduino/libraries folder.
+// Be sure to either restart the Arduino IDE or click
+// Edit -> Include Libaries -> Manage Libaries... to update paths
+#include <LiquidCrystal_I2C.h> 
+#include <Wire.h>
+
+//---------------------------------------------------------------
+// Forward Declarations
+
+// note uint8_t = unsigned (i.e. > 0) 8-bit integer
+// Microcontrollers of limited storage and dynamic memory should use this
+// type liberally to avoid unnecessary memory demands
+void printWord(const String& serialBuffer, uint8_t& indexSpace, uint8_t& lastSpace,
+  uint8_t& wordLength, uint8_t& currRow, uint8_t& offset);
+
+//---------------------------------------------------------------
+// I2C Bus to LCD Pins
+
+#define Rs_pin         0
+#define Rw_pin         1
+#define En_pin         2
+#define BACKLIGHT_PIN  3 
+#define D4_pin         4
+#define D5_pin         5
+#define D6_pin         6
+#define D7_pin         7
+
+//---------------------------------------------------------------
+// Other Constants
+
+#define LED_OFF        0
+#define LED_ON         1
+#define BAUD_RATE      9600
+#define CHARS_PER_LINE 16
+#define NUM_LINES      2
+#define DEVICE_NOT_FOUND -1
+
+//---------------------------------------------------------------
+// Globals
+//---------------------------------------------------------------
+
+// initialize the lcd display in setup 
+LiquidCrystal_I2C* lcd = NULL;
+String serialBuffer = "";
+const int FRAME_DELAY = 3000;
+
+//---------------------------------------------------------------
+// Initialization Code
+//---------------------------------------------------------------
+
+void setup()
+{
+  Serial.begin(BAUD_RATE);
+  uint8_t deviceAddress = findDeviceAddress();
+  // findDeviceAddress() DEVICE_NOT_FOUND if the SDA/SCL pins aren't set up correctly.
+  if (deviceAddress != DEVICE_NOT_FOUND) {
+      lcd = new LiquidCrystal_I2C(deviceAddress,
+        En_pin,
+        Rw_pin,
+        Rs_pin,
+        D4_pin,
+        D5_pin,
+        D6_pin,
+        D7_pin);      
+      lcd->begin(CHARS_PER_LINE, NUM_LINES);
+  // backlight pin, positive polarity
+  // If the LED flickers in and out, the likely fix is a change 
+  // of the polarity from positive to NEGATIVE
+      lcd->setBacklightPin(BACKLIGHT_PIN, POSITIVE);
+      lcd->setBacklight(LED_ON);
+      lcd->clear();
+      lcd->setCursor(0, 0);
+   }
+}
+
+// Because of course no one knows their own address these days...
+uint8_t findDeviceAddress() {
+  // I2C does 7-bit addressing.. 2^7
+  // Dr. Kwon would know this sooo fast. 
+  // "What is 01111111?"  "You are EE!  You should know this!"
+  uint8_t POSSIBLE_ADDRESSES = 128; 
+  Wire.begin();
+  uint8_t addressOfDevice = 1;
+  bool addressFound = false;
+  for (addressOfDevice; addressOfDevice < POSSIBLE_ADDRESSES; addressOfDevice++)
+  {
+    Wire.beginTransmission(addressOfDevice);
+    if (Wire.endTransmission() == 0)
+    {
+      addressFound = true;
+      break;
+    }
+  }
+  if (addressFound)
+  {
+    Serial.print("Device Address: ");
+    Serial.println(addressOfDevice);
+    return addressOfDevice;
+  }
+  else return DEVICE_NOT_FOUND;
+}
+
+//---------------------------------------------------------------
+// Some C/++ stuff for optimal chick magnetization
+//---------------------------------------------------------------
+
+// Bruce Willis and Joseph Gordon Levitt
+void loop()
+{
+  if (Serial.available())
+  {
+    lcd->clear();
+    lcd->setCursor(0, 0);
+    serialBuffer = Serial.readString();
+    printStringToLCD(serialBuffer);
+  }
+}
+
+// Prints strings of whatever length to the LCD display
+// every | one | take | lesson |
+// to | pass | by | const | reference
+// else | pay | the | runtime | and | execute | sllllooooooooooow . w. w. w.
+void printStringToLCD(const String& serialBuffer) {
+  // O(n) algorithm -> It's the best... around... no one's ever gonna bring it down...
+  uint8_t wordLength = 0, currRow = 0, lastSpace = 0, indexSpace = 0, offset = 0;
+  uint8_t strLength = serialBuffer.length();
+  char stringBuffer[strLength + 1]; serialBuffer.toCharArray(stringBuffer, strLength);
+  for (offset; offset < strLength + 1; offset++)
+  {
+    if (stringBuffer[offset] == ' ')
+    {
+      printWord(serialBuffer, indexSpace, lastSpace, wordLength, currRow, offset);
+    }
+    wordLength++;
+  }
+  printWord(serialBuffer, indexSpace, lastSpace, wordLength, currRow, offset);
+}
+
+
+// every | one | take | lesson |
+// you | can | pass | just| by | reference
+// to | manipulate | values
+void printWord(const String& serialBuffer, uint8_t& indexSpace, uint8_t& lastSpace,
+  uint8_t& wordLength, uint8_t& currRow, uint8_t& offset) {
+    // space enough for the word
+  if ((indexSpace + wordLength - 1) < CHARS_PER_LINE)
+  {
+    lcd->print(serialBuffer.substring(lastSpace, lastSpace + wordLength));
+  }
+    // not enough space for this word
+    // move to next line
+  else
+  {
+    if (currRow == 1) {
+      delay(FRAME_DELAY);
+      lcd->clear();
+    }
+    // shorthand syntax for comparison
+    currRow = currRow == 1 ? 0 : 1;
+    lcd->setCursor(0, currRow);
+    lcd->print(serialBuffer.substring(lastSpace + 1, lastSpace + wordLength));
+    indexSpace = 0;
+  }
+  indexSpace += wordLength;
+  lastSpace = offset;
+  wordLength = 0;
+}
+
